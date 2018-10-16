3e436bb962bb18a8e1ed7392d9952683b01f4d31
AlexNevado <pailex95@gmail.com>
2018-10-16 19:31:45 +0200

    Creadas resources y empezada funcion inicial

---
+++ src/main/java/DarkBot.java
@@ -0,0 +1,38 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+/**
+ * Created by Dark_Trainer on 16/10/2018.
+ */
+public class DarkBot extends TelegramLongPollingBot {
+
+
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText("Xiao eres feo");
+            try {
+                execute(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getBotUsername() {
+        return "darktrainer_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "774220943:AAGxsurG0bhttkpDsTDaW8wsTI3RysQ6PgQ";
+    }
+}
---
+++ src/main/java/InicializarDatos.java
@@ -0,0 +1,43 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * Created by Dark_Trainer on 16/10/2018.
+ */
+public class InicializarDatos {
+
+    public HashMap<String,String> initProperties() {
+        HashMap<String, String> mapProp = new HashMap<String, String>();
+        Properties prop = new Properties();
+        InputStream input = null;
+        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+        String urlResources = rootPath + "resources.properties";
+        try {
+            input = new FileInputStream(urlResources);
+            // load a properties file
+            prop.load(input);
+            Iterator it = prop.entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry pair = (Map.Entry) it.next();
+                mapProp.put(pair.getKey().toString(), pair.getValue().toString());
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return mapProp;
+    }
+}
